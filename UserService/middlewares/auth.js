1a1d016099c43ee75678b2b5d0602040f57adbb8
Hemza-Assebab <assebabhamza03@gmail.com>
2025-04-28 01:19:59 +0100

    Implemented authentication

---
+++ UserService/middlewares/auth.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json({ message: 'You are unauthorized !' });
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_ACCESS_TOKEN);
+        req.user = decoded;
+        next();
+    } catch (e) {
+        return res.status(403).json({ message: 'Invalid or expired access token !' });
+    }
+}
+
+module.exports = authMiddleware;
